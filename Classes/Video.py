82334470f79d5548dbd24a8410518806d670c6fe
Anthony Choi <anthony.choi204@gmail.com>
2026-04-26 15:21:05 +1000

    Updated supporting files.

--- Classes/Video.py
+++ Classes/Video.py
@@ -1,9 +1,10 @@
 class Video:
-    def __init__(self, title, vid_id, pl_item_id):
+    def __init__(self, title, vid_id, creator, pl_item_id):
         self.title = title
         self.vid_id = vid_id
+        self.creator = creator
         self.pl_item_id = pl_item_id
 
 
     def __str__(self):
-        return f"\nThe video {self.title} has the video ID and playlist item ID of {self.vid_id}:{self.pl_item_id}."
+        return f"\n\"{self.title}\" was made by {self.creator} and has the video ID and playlist item ID of {self.vid_id}:{self.pl_item_id}."
