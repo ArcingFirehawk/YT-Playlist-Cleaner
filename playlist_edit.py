234c2f00230e7d9086b22883e4ee089ce695dd92
Anthony Choi <anthony.choi204@gmail.com>
2026-04-29 14:30:11 +1000

    Updated playlist_edit.py to deal with unavailable videos.

--- playlist_edit.py
+++ playlist_edit.py
@@ -15,9 +15,10 @@ def get_env(env_var):
     return(os.getenv(env_var))
 
 
-# Extracts the API's output into the Video class.
+# Extracts the API's output into the Video class format.
 def extract(api_response):
-    vid_list = []
+    good_vid_list = []  # List containing public videos.
+    bad_vid_list = []   # List containing unavailable videos.
     length = api_response["pageInfo"]["resultsPerPage"]
 
 
@@ -30,33 +31,50 @@ def extract(api_response):
             vid_creator = api_response["items"][i]["snippet"]["videoOwnerChannelTitle"]
             pl_item_id = api_response["items"][i]["id"]
 
-            vid_list.append(Video(vid_title, vid_id, vid_creator, pl_item_id))
+            good_vid_list.append(Video(vid_title, vid_id, vid_creator, pl_item_id))
+        else:
+            vid_title = f"Unavailable{i + 1:02d}"
+            vid_id = api_response["items"][i]["contentDetails"]["videoId"]
+            vid_creator = "N/A"
+            pl_item_id = api_response["items"][i]["id"]
+
+            bad_vid_list.append(Video(vid_title, vid_id, vid_creator, pl_item_id))
+
     
-    return vid_list
+    return good_vid_list, bad_vid_list
 
 
 def main():
-    # Credential Vars.
     api_key_public = get_env("API_KEY")
     api_key_private = "Credentials/client_secret_file.json"
     old_pl_id = get_env("OLD_PLAYLIST_ID")
     new_pl_id = get_env("NEW_PLAYLIST_ID")
-
-    max_results = 3 # max is 50.
+    max_results = 3 # Max is 50.
     
+
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
     get_vids_response = get_videos.api_request(api_key_public, old_pl_id, max_results)
     
-    vid_list = extract(get_vids_response)
-    length = len(vid_list)
+    good_vid_list, bad_vid_list = extract(get_vids_response)
+    good_length = len(good_vid_list)
+    bad_length = len(bad_vid_list)
     # print(f"\n\n{vid_list[0].title}\n\n")
 
-    for i in range(length):
+    print("\n\n-----Bad Videos-----")
+
+    # if statement to clean up old playlist so that next API request has fewer "bad" videos.
+    if bad_length >= 0:
+        for i in range(bad_length):
+            # del_videos.api_request(api_key_private, bad_vid_list[i].pl_item_id)
+            print(bad_vid_list[i].title)
+
+    print("\n\n-----Good Videos-----")
+
+    for i in range(good_length):
         # add_videos.api_request(api_key_private, new_pl_id, vid_list[i].vid_id)
         # del_videos.api_request(api_key_private, vid_list[i].pl_item_id)
-
-        print(vid_list[i].title)
+        print(good_vid_list[i].title)
 
 
 
