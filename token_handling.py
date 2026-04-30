b2725eceffc0d15753f1be586f9eec430fe41851
Anthony Choi <anthony.choi204@gmail.com>
2026-04-30 21:01:40 +1000

    Changed token_handling.py to use functions.

--- token_handling.py
+++ token_handling.py
@@ -11,7 +11,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 import google_auth_oauthlib.flow
 import google.auth.exceptions
-import json
 
 
 
@@ -19,7 +18,7 @@ scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 token_file = "Credentials/token.json"
 
 
-# Checks if a refresh token already exists.
+# Checks if refresh token already exists.
 def check_token():
     credentials = None
 
@@ -34,13 +33,14 @@ def check_token():
             print(f"\n\nRefresh token expired, requesting authorization again. \nError: {error}.")
 
     # if statement to get a new token if it doesn't exist or is invalid.
-    if not credentials or credentials.token_state == "INVALID":
+    if not credentials or credentials.token_state == 3:
+        print(f"\n\nRefresh token expired, requesting authorization again.")
         credentials = get_token()
 
-    # return credentials
+    return credentials
 
 
-# Gets a new refresh token.
+# Gets new refresh token.
 def get_token():
     api_key_private = "Credentials/client_secret_file.json"
 
@@ -53,41 +53,8 @@ def get_token():
     return credentials
 
 
-# Loads the existing token.
-def load_token():
-    check_token()
-
-    with open(token_file, "r") as f:
-        credentials = json.load(f)
-        f.close()
-
-    return credentials
-
-
 def main():
-    credentials = None
-    api_key_private = "Credentials/client_secret_file.json"
-
-
-    # if statement that checks if the token already exists and works.
-    if os.path.exists(token_file):
-        try:
-            credentials = Credentials.from_authorized_user_file(token_file, scopes)
-            credentials.refresh(Request())
-        except google.auth.exceptions.RefreshError as error:
-            credentials = None
-            print(f"\n\nRefresh token expired, requesting authorization again. \nError: {error}.")
-
-    # if statement to get a new token if it doesn't exist or is invalid.
-    if not credentials or credentials.token_state == "INVALID":
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(api_key_private, scopes)
-        credentials = flow.run_local_server(port=0)
-
-    with open(token_file, "w") as f:
-        f.write(credentials.to_json())
-
-    return credentials
-    
+    check_token()
 
 
 if __name__ == '__main__':
