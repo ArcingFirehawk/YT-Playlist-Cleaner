deba7f873df49d3c7a6f46dd03d0322f92e42452
Anthony Choi <anthony.choi204@gmail.com>
2026-05-28 14:45:01 +1000

    Reverted unintentional variable change.

--- playlist_edit.py
+++ playlist_edit.py
@@ -14,7 +14,7 @@ def main():
     api_key_private = check_token()
     old_pl_id = get_env("OLD_PLAYLIST_ID")
     new_pl_id = get_env("NEW_PLAYLIST_ID")
-    max_results = 50 # Max is 50.
+    max_results = 3 # Max is 50.
 
 
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
